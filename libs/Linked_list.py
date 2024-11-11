a64690cccd1acc59b5ce8521f55c15700da57c9c
Kareem Ghazi <k.ghazi2005@gmail.com>
2024-11-11 20:27:35 +0200

    Add "add" & "remove head" functions

--- libs/Linked_list.py
+++ libs/Linked_list.py
@@ -2,29 +2,53 @@ class Node:
     def __init__(self, data):
         self.next = self.prev = None
         self.data = data
+        
 class Linked_list:
     def __init__(self):
         self.head = self.tail = None
         counter = 0
+    
     def isEmpty(self):
         pass
+    
     def add(self, data):
-        pass
+        newNode = Node(data)
+        
+        if self.isEmpty():
+            self.head = newNode
+        else:
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+    
     def append(self, data):
         pass
+    
     def insert(self, data, pos):
         pass
+    
     def search(self, data):
         pass
+    
     def traverse(self):
         pass
+    
     def removeHead(self):
-        pass
+        if self.isEmpty():
+            return
+        
+        temp = self.head
+        self.head = self.head.next
+        temp = None
+    
     def removeTail(self):
         pass
+    
     def removeAtItem(self, data):
         pass
+    
     def clear(self):
         pass
+    
     def removeAll(self, data):
         pass
