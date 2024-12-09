49ef45c7857f14259e62a567440e14c0293f59f3
Ser5io <yousefkilany10@gmail.com>
2024-12-09 19:19:29 +0200

    DONE stack

--- libs/stack.py
+++ libs/stack.py
@@ -4,22 +4,81 @@ class Stack:
     def __init__(self):
         self.top = None
         self.counter = 0
+        
     def isEmpty(self):
         return self.top == None
+    
+    def emptyMsg(self):
+        print('Empty list....!')
+        
     def peek(self):
-        if not self.is_empty():
-            return self.items[-1]
+        if self.isEmpty():
+            self.emptyMsg()
         else:
-            raise IndexError("peek from an empty stack")
+            return self.top.data
+        
     def getSize(self):
-        pass
+        return self.counter
+    
     def push(self,data):
-        self.items.append(data)
+        newNode = Node(data)
+        if self.isEmpty():
+            self.top = newNode
+        else:
+            newNode.next = self.top
+            self.top = newNode
+        
+        self.counter += 1
+        
     def pop(self):
-        pass
+        if self.isEmpty():
+            self.emptyMsg()
+        else:
+            temp = self.top
+            tempData = temp.data
+            self.top = self.top.next
+            self.counter -= 1
+            del temp
+            return tempData
     def display(self):
-        pass
-    def revese(self):
-        pass
+        if self.isEmpty():
+            self.emptyMsg()
+        else:
+            temp = self.top
+            print('[',end=' ')
+            while temp:
+                print(temp.data,end=' ')
+                temp = temp.next
+            print(']')
+            
     def clear(self):
-        pass
+        while self.top:
+            temp = self.top
+            self.top = self.top.next
+            del temp
+        self.top = None
+        self.counter = 0
+
+stack = Stack()
+print(stack.getSize())
+stack.push(10)
+print(stack.getSize())
+stack.push(20)
+print(stack.getSize())
+
+stack.push(30)
+print(stack.getSize())
+
+stack.push(40)
+print(stack.getSize())
+
+print(stack.pop())
+print(stack.getSize())
+
+print(stack.peek())
+stack.display()
+print(stack.getSize())
+
+stack.clear()
+print(stack.getSize())
+stack.display()
